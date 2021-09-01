c8279dffb0e4744631f85651e680cc5519454a17
peyo-code <mr233425@gmail.com>
2021-09-01 18:55:58 +0900

    feat(word):動的ルーティング

--- components/WordItemRow.vue
+++ components/WordItemRow.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <div v-for="item of items" :key="item" class="flex justify-center mt-4 mb-4">
+    <NuxtLink v-bind:to="{name:'word',query:{id:item}}" v-for="item of items" :key="item" class="flex justify-center mt-4 mb-4">
       <div class="folser-icon">
         <p>{{ item.icon }}</p>
       </div>
@@ -8,7 +8,7 @@
         <p>{{ item.title }}</p>
         <p>{{ item.date }}</p>
       </div>
-    </div>
+    </NuxtLink>
   </div>
 </template>
 
@@ -18,19 +18,19 @@
       return {
         items: [
           {
-            icon: "iconが入ります。" ,
+            icon: "1iconが入ります。" ,
             title: "タイトルが入ります",
-            date: "日付が入ります。"
+            date: "日付(1)が入ります。"
           },
           {
-            icon: "iconが入ります。" ,
+            icon: "2iconが入ります。" ,
             title: "タイトルが入ります",
-            date: "日付が入ります。"
+            date: "日付(2)が入ります。"
           },
           {
-            icon: "iconが入ります。" ,
+            icon: "3iconが入ります。" ,
             title: "タイトルが入ります",
-            date: "日付が入ります。"
+            date: "日付(3)が入ります。"
           }
         ],
       }
---
+++ pages/word.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <div class="text-center mt-4">
+      <p>{{ $route.query.id.icon }}</p>
+      <p>{{ $route.query.id.title }}</p>
+      <p>{{ $route.query.id.date }}</p>
+    </div>
+    <WordEdit />
+    <CreateWord />
+  </div>
+</template>
