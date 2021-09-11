76ab921bf2ff39a4dbd7e4db99ddab608d844e1a
peyo-code <mr233425@gmail.com>
2021-09-12 02:01:14 +0900

    fix(word):命名修正

--- pages/word.vue
+++ pages/word.vue
@@ -1,11 +1,12 @@
 <template>
   <div>
     <div class="text-center mt-4">
-      <p>{{ $route.query.id.icon }}</p>
       <p>{{ $route.query.id.title }}</p>
       <p>{{ $route.query.id.date }}</p>
     </div>
-    <WordEdit />
-    <CreateWord />
+    <div class="flex justify-center mt-4">
+      <EditWord />
+      <CreateWord />
+    </div>
   </div>
 </template>
