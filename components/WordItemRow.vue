cc8ca191387eb8c51bddee3f90f40cc864d53497
peyo-code <mr233425@gmail.com>
2021-09-18 19:38:48 +0900

    DBから単語帳のタイトルを取得して、画面に表示する

--- components/WordItemRow.vue
+++ components/WordItemRow.vue
@@ -1,12 +1,12 @@
 <template>
   <div>
-    <NuxtLink v-bind:to="{name:'word',query:{id:item}}" v-for="item of items" :key="item" class="flex justify-center mt-4 mb-4">
-      <div class="folser-icon">
+    <NuxtLink v-bind:to="{name:'word',query:{id:information}}" v-for="information of informations" :key="information" class="flex justify-center mt-4 mb-4">
+      <div class="folser-icon w-12">
         <span class="material-icons">folder</span>
       </div>
       <div class="MultilineRowMetadataStructure-rightChildren">
-        <p>{{ item.title }}</p>
-        <p>{{ item.date }}</p>
+        <p>{{ information.id }}</p>
+        <p>{{ information.name }}</p>
       </div>
     </NuxtLink>
   </div>
@@ -30,7 +30,15 @@
             date: "日付(3)が入ります。"
           }
         ],
+        informations: null
       }
     },
+  mounted () {
+      this.$axios.get('http://localhost:5000/api')
+      .then(response => {
+        console.log(response);
+        this.informations = response.data
+      })
+  },
   }
 </script>
